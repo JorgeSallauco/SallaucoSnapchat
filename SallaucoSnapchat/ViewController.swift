198647a8ab9adc4e634de1af6b70a155abec4530
Jorge <jorge.sallauco@tecsup.edu.pe>
2024-05-20 13:24:09 -0500

    Initial Commit

---
+++ SallaucoSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SallaucoSnapchat
+//
+//  Created by Sallauco Jorge 2 on 20/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
